f2f5c514f2f2fe812f5617f2d46c7f2e7b3e4be3
Kiki Zhang <kiki.zhang@vici.com>
2022-11-01 15:52:18 +0800

    add eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { NodeType } from "@yangzw/bruce-us/dist/node";
 
-console.log(NodeType());
-
 // read file
 
 import { readFileSync } from "fs";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+
+console.log(NodeType());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,4 +14,4 @@ console.log(__filename, __dirname);
 
 const json = readFileSync(join(__dirname, "./info.json"));
 const info = JSON.parse(json);
-console.log('info== ', info)
+console.log("info== ", info);
